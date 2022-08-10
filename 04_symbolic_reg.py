9448d818efc5b17d0aef1d6f372285cf6f3b4960
Johniel Bocacao <accounts@johniel.nz>
2022-08-10 19:33:11 +1200

    Finish part 4 with 3 examples

--- 04_symbolic_reg.py
+++ 04_symbolic_reg.py
@@ -7,16 +7,19 @@ Inspired from https://github.com/DEAP/deap/blob/12ed30e0b8fc95c155f15dfce468b79e
 import math
 import operator
 import random
+from pprint import pprint
 
 import numpy
 import numpy as np
+import pandas as pd
 from deap import creator, base, tools, algorithms, gp
+from deap.algorithms import varOr
 
 verbose = True
-mu = 100
-p_cross = 0.85
-p_mutate = 0.15
-n_elite = 2
+mu = 1000
+p_cross = 0.8
+p_mutate = 0.2
+n_elite = int(mu*0.05)
 epochs = 100
 init_min_depth = 2
 init_max_depth = 6
@@ -26,7 +29,7 @@ mutate_max_depth = 6
 
 
 def createPrimitiveSet():
-    primitives = gp.PrimitiveSetTyped("main", [float], float, "in")
+    primitives = gp.PrimitiveSetTyped("main", [float], float, "x")
     # Numeric operators
     primitives.addPrimitive(operator.add, [float, float], float)
     primitives.addPrimitive(operator.sub, [float, float], float)
@@ -43,7 +46,7 @@ def createPrimitiveSet():
     primitives.addPrimitive(protectedDiv, [float, float], float)
 
     def square(x):
-        return x**2
+        return x ** 2
 
     # Adding a power operator gave it too much power it couldn't handle (negative operands => complex)
     primitives.addPrimitive(square, [float], float)
@@ -61,6 +64,19 @@ def createPrimitiveSet():
     return primitives
 
 
+def evaluate(individual, points):
+    f = toolbox.compile(expr=individual)
+
+    def trueF(x):
+        if x > 0:
+            return 1 / x + math.sin(x)
+        return 2 * x + x ** 2 + 3.0
+
+    sse = math.fsum([(f(x) - trueF(x)) ** 2 for x in points])
+    # Return as a tuple for DEAP
+    return sse / len(points),
+
+
 def createToolbox():
     toolbox = base.Toolbox()
     # Generate individual using half (full) and half (grow) method
@@ -70,23 +86,8 @@ def createToolbox():
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
     toolbox.register("compile", gp.compile, pset=primitives)
 
-    def evaluate(individual, points):
-        f = toolbox.compile(expr=individual)
-
-        def trueF(x):
-            if x > 0:
-                return 1 / x + math.sin(x)
-            return 2 * x + x ** 2 + 3.0
-
-        try:
-            sse = math.fsum([(f(x) - trueF(x)) ** 2 for x in points])
-        except ValueError:
-            print(individual)
-        # Return as a tuple for DEAP
-        return sse / len(points),
-
     # Evaluate over x = {-10,-9,-8,...,8,9,10}
-    toolbox.register("evaluate", evaluate, points=[x / 10. for x in range(-10, 10)])
+    toolbox.register("evaluate", evaluate, points=[x / 5. for x in range(-25, 50)])
     # Tournament selection, 3 participants
     toolbox.register("select", tools.selTournament, tournsize=3)
     toolbox.register("mate", gp.cxOnePoint)
@@ -111,25 +112,85 @@ toolbox = createToolbox()
 
 def main(seed=None):
     random.seed(seed)
+    np.random.seed(seed)
 
     pop = toolbox.population(n=mu)
     hof = tools.HallOfFame(1)
 
     stats_fit = tools.Statistics(lambda ind: ind.fitness.values)
-    stats_size = tools.Statistics(len)
-    mstats = tools.MultiStatistics(fitness=stats_fit, size=stats_size)
+    mstats = tools.MultiStatistics(fitness=stats_fit)
+    mstats.register("min", numpy.min)
     mstats.register("avg", numpy.mean)
     mstats.register("std", numpy.std)
-    mstats.register("min", numpy.min)
-    mstats.register("max", numpy.max)
 
-    pop, log = algorithms.eaSimple(pop, toolbox, p_cross, p_mutate, epochs, stats=mstats,
-                                   halloffame=hof, verbose=True)
-    print(hof)
+    # Copied from DEAP source code (eaMuPlusLambda) with modifications indicated
+    log = tools.Logbook()
+    log.header = ['gen', 'nevals'] + (mstats.fields if mstats else [])
 
-    # print log
+    # Evaluate the individuals with an invalid fitness
+    invalid_ind = [ind for ind in pop if not ind.fitness.valid]
+    fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+    for ind, fit in zip(invalid_ind, fitnesses):
+        ind.fitness.values = fit
+
+    hof.update(pop)
+
+    record = mstats.compile(pop)
+    log.record(gen=0, nevals=len(invalid_ind), **record)
+    print(log.stream)
+
+    # Begin the generational process
+    for gen in range(1, epochs + 1):
+        # Vary the population
+        offspring = varOr(pop, toolbox, mu, p_cross, p_mutate)
+
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+
+        # Update the hall of fame with the generated individuals
+        hof.update(offspring)
+
+        # Select the next generation pop
+        pop[:] = tools.selBest(offspring, mu - n_elite) + tools.selBest(pop, n_elite)
+
+        # Update the statistics with the new pop
+        record = mstats.compile(pop)
+        log.record(gen=gen, nevals=len(invalid_ind), **record)
+        print(log.stream)
+
+    best_program = toolbox.compile(expr=hof.items[0])
+    print("=== Best f(x) over [-5, 10] + 0.2 ===")
+    print("Fitness =", hof.keys[0])
+    print(str(hof.items[0]))
+    pprint([best_program(x / 5.) for x in range(-25, 50)])
+
+    nodes, edges, labels = gp.graph(hof.items[0])
+    for i in labels:
+        if isinstance(labels[i], float):
+            labels[i] = round(labels[i], 2)
+
+    # Visualise tree
+    import pygraphviz as pgv
+
+    g = pgv.AGraph()
+    g.add_nodes_from(nodes)
+    g.add_edges_from(edges)
+    g.layout(prog="dot")
+
+    for i in nodes:
+        n = g.get_node(i)
+        n.attr["label"] = labels[i]
+
+    g.draw("out/part4/"+str(seed)+"_tree.pdf")
+    log_df = pd.DataFrame(log.chapters["fitness"])
+    log_df.to_csv("out/part4/"+str(seed)+"_log.csv")
+
     return pop, log, hof
 
 
 if __name__ == '__main__':
-    main(0)
+    for seed in range(3):
+        main(seed)
